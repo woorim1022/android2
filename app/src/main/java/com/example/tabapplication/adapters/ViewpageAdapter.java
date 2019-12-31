526d49b81af225cab05d77e0c188ba346f357f99
Chungyezun <maple0729@kaist.ac.kr>
2019-12-31 22:34:27 +0900

    sdafdsaadsf

--- app/src/main/java/com/example/tabapplication/adapters/ViewpageAdapter.java
+++ app/src/main/java/com/example/tabapplication/adapters/ViewpageAdapter.java
@@ -56,8 +56,14 @@ public class ViewpageAdapter extends PagerAdapter {
                 num = i;
             }
         }
-        imageView.setImageResource(imageIDs[position + num]);
-        textView.setText(fileNames[position + num]);
+        if(position + num <=19) {
+            imageView.setImageResource(imageIDs[position + num]);
+            textView.setText(fileNames[position + num]);
+        }
+        else{
+            imageView.setImageResource(imageIDs[position-1]);
+            textView.setText(fileNames[position-1]);
+        }
         container.addView(v);
         return v;
     }
